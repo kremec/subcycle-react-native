dc603ac0bfd8951401b55e08e8804ad2c54d4f63
subsurface <klemen.remec@gmail.com>
2024-08-24 13:16:59 +0200

    Setup development and production environments

---
+++ app.config.js
@@ -0,0 +1,44 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+
+export default {
+    expo: {
+        name: IS_DEV ? "subcycle (Dev)" : "subcycle",
+        slug: "subcycle",
+        scheme: "subcycle",
+        version: "1.0.1",
+        orientation: "portrait",
+        icon: "./assets/icon.png",
+        userInterfaceStyle: "automatic",
+        android: {
+            adaptiveIcon: {
+                foregroundImage: "./assets/adaptive-icon.png",
+                backgroundColor: "#ffffff"
+            },
+            splash: {
+                image: "./assets/splash.png",
+                resizeMode: "contain",
+                backgroundColor: "#ffffff",
+                dark: {
+                    image: "./assets/splash.png",
+                    resizeMode: "contain",
+                    backgroundColor: "#000000"
+                }
+            },
+            package: IS_DEV ? "com.subbyte.subcycle.dev" : "com.subbyte.subcycle"
+        },
+        web: {
+            favicon: "./assets/favicon.png"
+        },
+        plugins: [
+            "expo-router"
+        ],
+        extra: {
+            router: {
+                origin: false
+            },
+            eas: {
+                projectId: "48d6445a-942c-474d-be16-5d46d6f25603"
+            }
+        }
+    }
+};
